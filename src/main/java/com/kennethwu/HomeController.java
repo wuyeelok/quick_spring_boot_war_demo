79ddc3dca2c5c59d79167291f3ba790e7a038527
SD4A <sd4a@INF62s-iMac.local>
2019-10-17 10:41:00 +0800

    Config: Work in tomcat but not in wildfly

---
+++ src/main/java/com/kennethwu/HomeController.java
@@ -0,0 +1,15 @@
+package com.kennethwu;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		System.out.println("calling home method");
+		return "index";
+	}
+
+}

d092937489ccc0290415c57a475749e1a49462e1
wuyeelok <wuyeelok@gmail.com>
2019-11-08 15:06:42 +0800

    Refactor: Use actual value from properties

--- src/main/java/com/kennethwu/HomeController.java
+++ src/main/java/com/kennethwu/HomeController.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,23 +12,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+	private final String jdbcUrl;
+	private final String dbUser;
+	private final String dbPass;
+
+	public HomeController(@Value("${spring.datasource.url}") String jdbcUrl,
+			@Value("${spring.datasource.username}") String dbUser,
+			@Value("${spring.datasource.password}") String dbPass) {
+
+		// Values are defined in application.properties
+
+		this.jdbcUrl = jdbcUrl;
+		this.dbUser = dbUser;
+		this.dbPass = dbPass;
+	}
+
 	@GetMapping("/")
 	public String home(ModelMap model) {
 
 		// Test JDBC
 		Connection myConn = null;
 		try {
-			String jdbcUrl = "jdbc:mysql://localhost:3306/kenneth_alien?useSSL=false&serverTimezone=UTC";
 
-			String dbUser = "kenken";
+			myConn = DriverManager.getConnection(this.jdbcUrl, this.dbUser, this.dbPass);
 
-			String dbPass = "12345678";
+			model.addAttribute("jdbcUrl", this.jdbcUrl);
 
-			myConn = DriverManager.getConnection(jdbcUrl, dbUser, dbPass);
-
-			model.addAttribute("jdbcUrl", jdbcUrl);
-
-			model.addAttribute("dbUser", dbUser);
+			model.addAttribute("dbUser", this.dbUser);
 
 		} catch (Exception exc) {
 			exc.printStackTrace();
